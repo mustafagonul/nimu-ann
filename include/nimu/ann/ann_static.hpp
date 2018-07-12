64c7fee523f321fd3c4860d5f963fae4ab7cc118
Mustafa Gönül <mustafagonul@gmail.com>
2018-07-12 16:31:51 +0300

    - test directory introduced

--- include/nimu/ann/ann_static.hpp
+++ include/nimu/ann/ann_static.hpp
@@ -12,18 +12,19 @@ class ann_static {
 private:
   using value_type = Type;
   using size_type = size_t;
-  using input_size = Input;
-  using layer_size = Layer;
-  using hidden_size = Hidden;
-  using output_size = Output;
+  static constexpr size_type input_size = Input;
+  static constexpr size_type layer_size = Layer;
+  static constexpr size_type hidden_size = Hidden;
+  static constexpr size_type output_size = Output;
 
-  static_assert(std::is_floating_point<value_type>, "Value type must be floating point");
+  static_assert(std::is_floating_point<value_type>::value, "Value type must be floating point");
+  // static_assert(std::is_floating_point<value_type>::value);
 
 public:
 
 private:
   using input_type = std::array<value_type, input_size>;
-  using layer_type = std::array<value_type, hidden_type>;
+  using layer_type = std::array<value_type, hidden_size>;
   using layers_type = std::array<layer_type, layer_size>;
   using output_type = std::array<value_type, output_size>;
 
---
+++ test/ann_static.cpp
@@ -0,0 +1,15 @@
+#define BOOST_TEST_MODULE nimu_ann_test_ann_static
+#include <boost/test/unit_test.hpp>
+
+#include <nimu/ann/ann_static.hpp>
+
+
+BOOST_AUTO_TEST_SUITE(ann_static)
+
+BOOST_AUTO_TEST_CASE(constructor_1)
+{
+  BOOST_TEST(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
