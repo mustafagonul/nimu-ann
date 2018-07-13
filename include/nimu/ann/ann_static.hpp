899b46855512c9fd2b5297be2e61f77584a85c07
Mustafa Gönül <mustafagonul@gmail.com>
2018-07-13 15:47:49 +0300

    - ann_static.hppp updates

--- include/nimu/ann/ann_static.hpp
+++ include/nimu/ann/ann_static.hpp
@@ -9,18 +9,48 @@ namespace nimu {
 
 template <typename Type, size_t Input, size_t Layer, size_t Hidden, size_t Output>
 class ann_static {
-private:
+public:
   using value_type = Type;
+  using reference_type = value_type&;
+  using const_reference_type = value_type const&;
   using size_type = size_t;
+
   static constexpr size_type input_size = Input;
   static constexpr size_type layer_size = Layer;
   static constexpr size_type hidden_size = Hidden;
   static constexpr size_type output_size = Output;
 
   static_assert(std::is_floating_point<value_type>::value, "Value type must be floating point");
-  // static_assert(std::is_floating_point<value_type>::value);
 
 public:
+  ann_static() = default;
+  ~ann_static() = default;
+
+  ann_static(ann_static const&) = default;
+  ann_static(ann_static &&) = delete;
+
+  ann_static const& operator=(ann_static const &) = default;
+  ann_static&& operator=(ann_static&&) = delete;
+
+
+  const_reference_type
+  input(size_type) const;
+
+  reference_type
+  input(size_type);
+
+  const_reference_type
+  output(size_type) const;
+
+  reference_type
+  output(size_type);
+
+  const_reference_type
+  node(size_type, size_type) const;
+
+  reference_type
+  node(size_type, size_type);
+
 
 private:
   using input_type = std::array<value_type, input_size>;
@@ -28,9 +58,9 @@ private:
   using layers_type = std::array<layer_type, layer_size>;
   using output_type = std::array<value_type, output_size>;
 
-  input_type  input;
-  layers_type layers;
-  output_type output;
+  input_type  input_array;
+  layers_type layer_array;
+  output_type output_array;
 };
 
 
